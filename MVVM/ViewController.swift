c9d7ae95af07aaa436c27f1edc48a138110bde7f
fixityy <belov.roman.yur@gmail.com>
2022-04-18 18:06:02 +0300

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Roman Belov on 18.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
